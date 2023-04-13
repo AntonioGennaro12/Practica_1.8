0c833cf137f72f1976db1adddfcefc1137d6d5eb
Antonio Gennaro <antgennaro@hotmail.com>
2023-04-13 19:22:27 -0300

    Modificaciones en presentación

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,39 +1,43 @@
 /* 
 Operadores de Condición 
 */
+console.log("\n ** Verificación de Condicioes Simples y Dobles **\n");
+console.log("Cond. #1  " + (4 < 4) );
+console.log("Cond. #2  " + (23 == 33) );
+console.log("Cond. #3  " + (37 >= -3) );
+console.log("Cond. #4  " + ("Joel" == "Joel") );
+console.log("Cond. #5  " + ("joel" == "Joel") );
+console.log("Cond. #6  " + ('!"$:;14;' != "123qwerty") );
+console.log("Cond. #7  " + (("Joel" == "Joel") && ('!"$:;14;' == '!"$:;14;')) );
+console.log("Cond. #8  " + (("Joel" == "joel") || ('!"$:;14;' == '!"$:;14;')) );
+console.log("Cond. #9  " + (("joel" != "Joel") && ('!"$:;14;' != "123qwerty")) );
+console.log("Cond. #10 " + (("joel" != "Joel") && !('!"$:;14;' != "123qwerty"))+(" Igual que #9 con 2da Condición Negada") ); /* niego la 2da expresión */
 
-console.log(4 < 4);
-console.log(23 == 33);
-console.log(37 >= -3);
-console.log("Joel" == "Joel");
-console.log("joel" == "Joel");
-console.log('!"$:;14;' != "123qwerty");
-console.log(("Joel" == "Joel") && ('!"$:;14;' == '!"$:;14;'));
-console.log(("Joel" == "joel") || ('!"$:;14;' == '!"$:;14;'));
-console.log(("joel" != "Joel") && ('!"$:;14;' != "123qwerty"));
-console.log(("joel" != "Joel") && !('!"$:;14;' != "123qwerty")); /* niego la 2da expresión */
-/* Usando "if" y "else" fuera del console.log */
-console.log("Verfico Edad usando if y else fuera del console.log"); 
+console.log("\n*** Verficación Edad de Usuario ***"); 
+/* Verifico Edad Usando "if" y "else" fuera del console.log */
 const EDAD_MINIMA = 18;
 const EDAD_MAXIMA = 100;  /* Debe ser menor que este número */  
-let edadUsuarioIngresado = 25;
+let edadUsuarioIngresado = 23;
+console.log("Edad de Usuario Ingresada = " +edadUsuarioIngresado+ ".-\n");
+console.log('\nVerfico Edad del Usuario usando "if" y "else" fuera del "console.log"'); 
 if((edadUsuarioIngresado >= EDAD_MINIMA) && (edadUsuarioIngresado < EDAD_MAXIMA)) {
-    console.log ("Edad de Usuario Válida!!"); }
+    console.log ("Chk Edad #1 = Edad de Usuario Válida!!"); }
 else {
-    console.log ("Edad de Usuario No válida!!"); }  
-/* Dentro del console.log */
-console.log("Condición resuelta dentro del console.log");
-console.log(((edadUsuarioIngresado >= EDAD_MINIMA) && (edadUsuarioIngresado < EDAD_MAXIMA)) ? 
-    "Edad de Usuario Válida!!!" :  "Edad de Usuario No válida!!!");
-console.log(((edadUsuarioIngresado >= EDAD_MINIMA) || (edadUsuarioIngresado < EDAD_MAXIMA)) ? 
-    "Edad de Usuario Válida!!!" :  "Edad de Usuario No válida!!!");
-console.log(((edadUsuarioIngresado <= EDAD_MINIMA) || (edadUsuarioIngresado < EDAD_MAXIMA)) ? 
-    "Edad de Usuario Válida!!!" :  "Edad de Usuario No válida!!!");
-console.log(((edadUsuarioIngresado < EDAD_MINIMA) && (edadUsuarioIngresado > EDAD_MAXIMA)) ? 
-    "Edad de Usuario Válida!!!" :  "Edad de Usuario No válida!!!");
-console.log(((edadUsuarioIngresado < EDAD_MINIMA) || (edadUsuarioIngresado > EDAD_MAXIMA)) ? 
-    "Edad de Usuario Válida!!!" :  "Edad de Usuario No válida!!!");
+    console.log ("Chk Edad #1 = Edad de Usuario No válida!!"); }  
 
+/* Dentro del console.log */
+console.log('\nVerifico Edad del Usuario dentro del comando "console.log"');
+console.log(((edadUsuarioIngresado >= EDAD_MINIMA) && (edadUsuarioIngresado < EDAD_MAXIMA)) ? 
+    "Chk Edad #1 = Edad de Usuario Válida!!!" :  "Chk Edad #1 = Edad de Usuario No válida!!!");
+console.log(((edadUsuarioIngresado >= EDAD_MINIMA) || (edadUsuarioIngresado < EDAD_MAXIMA)) ? 
+    "Chk Edad #2 = Edad de Usuario Válida!!!" :  "Chk Edad #2 = Edad de Usuario No válida!!!");
+console.log(((edadUsuarioIngresado <= EDAD_MINIMA) || (edadUsuarioIngresado < EDAD_MAXIMA)) ? 
+    "Chk Edad #3 = Edad de Usuario Válida!!!" :  "Chk Edad #3 = Edad de Usuario No válida!!!");
+console.log(((edadUsuarioIngresado < EDAD_MINIMA) && (edadUsuarioIngresado > EDAD_MAXIMA)) ? 
+    "Chk Edad #4 = Edad de Usuario Válida!!!" :  "Chk Edad #4 = Edad de Usuario No válida!!!");
+console.log(((edadUsuarioIngresado < EDAD_MINIMA) || (edadUsuarioIngresado > EDAD_MAXIMA)) ? 
+    "Chk Edad #5 = Edad de Usuario Válida!!!" :  "Chk Edad #5 = Edad de Usuario No válida!!!");
+console.log("\n ** Fin **\n");
     /*Fin */
 
 
